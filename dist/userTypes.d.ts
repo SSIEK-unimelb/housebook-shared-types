a863e00a41f1aa916482ab9008310bb345a332c0
ayemyatmin <ayemyatmin9999@gmail.com>
2026-03-01 20:50:16 +1100

    Added user interface and added some fields in owner interface

--- dist/userTypes.d.ts
+++ dist/userTypes.d.ts
@@ -1,6 +1,20 @@
 export interface Owner {
     ownerId: string;
+    userId: string;
     firstName: string;
     lastName: string;
     email: string;
+    phone: string;
+    status: string;
+    createdAt: string;
+}
+
+export interface User {
+    userId: string;
+    createdAt: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    email: string;
+    status: string;
 }
